af8d8c2452228fab8e3f4899316fd0030c86f30b
Tuan Doan <td35.98@gmail.com>
2017-08-25 05:29:45 +0700

    homw page

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,10 +18,11 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from blog import views
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', views.LandingPageView.as_view(), name="landing"),
+    url(r'^$', TemplateView.as_view(template_name='blog/index.html'), name="home"),
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls'))
 ]
