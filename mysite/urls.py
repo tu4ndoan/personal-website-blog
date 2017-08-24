c9ca441f02b3acb8f623e14f362811d84a239f2b
Tuan Doan <td35.98@gmail.com>
2017-08-25 05:46:27 +0700

    ad landing page

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,12 +17,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from blog import views
 from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='blog/index.html'), name="home"),
+    url(r'^$', TemplateView.as_view(template_name='blog/landing.html'), name="home"),
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls'))
 ]
