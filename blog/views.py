fc21e9a149f9970907fe9cc8a986cfe36b30ea12
Tuan Doan <td35.98@gmail.com>
2017-08-25 12:41:34 +0700

    test

--- blog/views.py
+++ blog/views.py
@@ -71,7 +71,7 @@ def add_comment_to_post(request, pk):
             comment = form.save(commit=False)
             comment.post = post
             comment.save()
-            return redirect('post_detail', {'pk':pk})
+            return redirect('post_detail', pk=pk)
     else:
         form = CommentForm()
     return render(request, 'blog/add_comment_to_post.html', {'form': form})
@@ -79,13 +79,13 @@ def add_comment_to_post(request, pk):
 
 @login_required
 def comment_approve(request, pk):
-    comment = get_object_or_404(Comment, {'pk':pk})
+    comment = get_object_or_404(Comment, pk=pk)
     comment.approve()
-    return redirect('post_detail', {'pk':pk})
+    return redirect('post_detail', pk=pk)
 
 
 @login_required
 def comment_remove(request, pk):
-    comment = get_object_or_404(Comment, {'pk':pk})
+    comment = get_object_or_404(Comment, pk=pk)
     comment.delete()
-    return redirect('post_detail', {'pk':pk})
+    return redirect('post_detail', pk=pk)
