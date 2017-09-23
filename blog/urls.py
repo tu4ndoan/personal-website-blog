e28a20793a7ed0c29802bd8de5acfa8fa51eeb81
Tuan Doan <td35.98@gmail.com>
2017-09-23 07:16:53 +0700

    update images' style format

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import url
 app_name = 'blog'
 
 urlpatterns = [
-
     url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^post/add/$', views.PostCreate.as_view(), name='post-add'),
