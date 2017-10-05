a6e3a07bb8d039daf00442415c9daa24b95d5cab
Tuan Doan <td35.98@gmail.com>
2017-10-05 12:29:02 +0700

    debug false comments badge, hook font

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.template.defaultfilters import slugify
 
 
 class BlogPost(models.Model):
@@ -9,15 +10,24 @@ class BlogPost(models.Model):
     content = models.TextField()
     feature_image = models.CharField(max_length=200, default='none')
     hooker = models.TextField()
-
-    def get_absolute_url(self):
-        return reverse('blog:detail', kwargs={'pk': self.pk})
+    #slug = models.SlugField(unique=True, default=None)
 
     def __str__(self):
         return self.post_title
 
+    @models.permalink
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    #def save(self, *args, **kwargs):
+    #    if not self.slug:
+    #        self.slug = slugify(self.title)
+    #    super(BlogPost, self).save(*args, **kwargs)
+
+
     def get_comment_count(self):
         return self.comments.filter(post=self).count()
+
 
 class Comment(models.Model):
     post = models.ForeignKey(BlogPost, related_name='comments')
